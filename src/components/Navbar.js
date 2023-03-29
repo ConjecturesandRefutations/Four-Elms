08dff34bb369302267fad358e740d98d04f88ecc
Alfie Collins <alfiecollins44@gmail.com>
2023-03-29 17:37:29 +0100

    changes to navbar spacing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,9 +25,9 @@ return(
               </svg>              
         </div>
     </div>
-    <ul class={`text-sm mt-6 ${menuOpen ? "" : "hidden"} inline-block md:block`} id="menu">
-        <li class="text-gray-700 py-1">
-            <a href="#" class="px-4 flex justify-end mt-4">
+    <ul class={`text-sm ${menuOpen ? "" : "hidden"} inline-block md:block`} id="menu">
+        <li class="text-gray-700">
+            <a href="#" class="px-4 flex justify-end">
                 <span> {lang==='english' ? 'About Four Elms' : 'Acerca de Four Elms'}</span>
             </a>
         </li>
@@ -36,52 +36,48 @@ return(
                 <span> {lang==='english' ? 'Check-in and Check-out Procedures' : 'Procedimientos de registro y salida'}</span>
             </a>
         </li>
-        <li class="py-1">
-            <a href="#" class="px-4 flex justify-end">
-            </a>
-        </li>
         <li class="py-2">
-            <a href="#" class="px-4 flex justify-end">
+            <a href="#" class="px-4 flex justify-end mt-4">
                 <span> {lang==='english' ? 'Contact and emergency information' : 'Información de contacto y emergencia'}</span>            
             </a>
         </li>
         <li class="py-2">
-            <a href="#" class="px-4 flex justify-end">
+            <a href="#" class="px-4 flex justify-end mt-4">
                 <span>{lang==='english' ? 'Alarm system and locking up' : 'Sistema de alarma y bloqueo'}</span>          
             </a>
         </li>
         <li class="py-2">
-            <a href="#" class="px-4 flex justify-end">
+            <a href="#" class="px-4 flex justify-end mt-4">
                 <span>{lang==='english' ? 'Instructions for appliances and electronics' : 'Instrucciones para electrodomésticos y electrónica'}</span>              
             </a>
         </li>
         <li class="py-2">
-            <a href="#" class="px-4 flex justify-end">
+            <a href="#" class="px-4 flex justify-end mt-4">
                 <span>{lang==='english' ? 'Towels, linen & kitchen supplies' : 'Toallas, ropa de cama y utensilios de cocina'}</span>               
             </a>
         </li>
         <li class="py-2">
-            <a href="#" class="px-4 flex justify-end">
+            <a href="#" class="px-4 flex justify-end mt-4">
                 <span>{lang==='english' ? 'Waste disposal and recycling' : 'Eliminación y reciclaje de residuos'}</span>                      
             </a>
         </li>
         <li class="py-2">
-            <a href="#" class="px-4 flex justify-end">
+            <a href="#" class="px-4 flex justify-end mt-4">
                 <span>{lang==='english' ? 'Information on the local area' : 'Información sobre el área local'}</span>               
             </a>
         </li>
         <li class="py-2">
-            <a href="#" class="px-4 flex justify-end">
+            <a href="#" class="px-4 flex justify-end mt-4">
                 <span>{lang==='english' ? 'House rules & contract' : 'Reglas de la casa y contrato'}</span>              
             </a>
         </li>
         <li class="py-2">
-            <a href="#" class="px-4 flex justify-end">
+            <a href="#" class="px-4 flex justify-end mt-4">
                 <span>{lang==='english' ? 'House cleaning' : 'Limpieza de la casa'}</span>             
             </a>
         </li>
         <li class="py-2">
-            <a href="#" class="px-4 flex justify-end">
+            <a href="#" class="px-4 flex justify-end mt-4">
                 <span>{lang==='english' ? 'Feedback' : 'Retroalimentación'}</span>               
             </a>
         </li>
