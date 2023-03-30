719477d3d324988950a689a97fa82d37cdab7c2a
Alfie Collins <alfiecollins44@gmail.com>
2023-03-30 16:14:28 +0100

    feedback lang and style

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,15 @@
+import { useContext } from 'react'
+import { LangContext } from '../context/language.context';
+
 import Navbar from "../components/Navbar";
 
 function Feedback(){
 
+     const { lang } = useContext(LangContext);
+
 return(
 
 <>
-
-
 
 <div class="grid md:grid-cols-4">
 
@@ -17,10 +20,10 @@ return(
 <div class='md:pt-10' id="container">
 
 <form action="https://formsubmit.co/ccollins@i-realise.co.uk" method="POST" > 
-     <input type="text" name="name" placeholder="your name" required/>
-     <input type="email" name="email" placeholder="your email" required/>
-     <input type="text" name="comments" placeholder="your feedback" required/>
-     <button className="button" type="submit">Send</button>
+     <input type="text" name="name" placeholder={lang==='english' ? "your name" : 'te llamas'} required/>
+     <input type="email" name="email" placeholder={lang==='english' ? "your email" : 'su correo electrónico'} required/>
+     <input type="text" name="comments" placeholder={lang==='english' ? "your feedback" : 'sus comentarios'} required/>
+     <button className="button" type="submit">{lang==='english' ? 'Send' : 'Enviar'}</button>
 </form>
 
 </div>
