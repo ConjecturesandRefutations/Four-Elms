d9b55f2380329b52143065f225cc0431e97d004b
Alfie Collins <alfiecollins44@gmail.com>
2023-04-01 00:09:49 +0100

    more changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,29 +50,29 @@ return(
               </svg>              
         </div>
     </div>
-    <ul class={`text-sm ${menuOpen ? "" : "hidden"} inline-block md:block mt-1 md:mt-4`} id="menu">
+    <ul class={`text-sm ${menuOpen ? "" : "hidden"} inline-block md:block mt-1 md:mt-3`} id="menu">
         <li class="text-gray-700 my-1">
            <Link to={'/'} activeClassName="active" className="px-4 flex justify-end" onClick={handleMenuClick}>
                 <span> {lang==='english' ? 'About Four Elms' : 'Acerca de Four Elms'}</span>
             </Link>
         </li>
         <li class="text-gray-700 py-2">
-            <Link to={'/check-in-out'} class="px-4 flex justify-end mt-1 md:mt-4" onClick={handleMenuClick}>
+            <Link to={'/check-in-out'} class="px-4 flex justify-end mt-1 md:mt-3" onClick={handleMenuClick}>
                 <span> {lang==='english' ? 'Check-in and Check-out Procedures' : 'Procedimientos de registro y salida'}</span>
             </Link>
         </li>
         <li class="py-2">
-            <Link to={'/contact'} class="px-4 flex justify-end mt-1 md:mt-4" onClick={handleMenuClick}>
+            <Link to={'/contact'} class="px-4 flex justify-end mt-1 md:mt-3" onClick={handleMenuClick}>
                 <span> {lang==='english' ? 'Contact and emergency information' : 'Información de contacto y emergencia'}</span>            
             </Link>
         </li>
         <li class="py-2">
-            <Link to={'/alarm'} class="px-4 flex justify-end mt-1 md:mt-4" onClick={handleMenuClick}>
+            <Link to={'/alarm'} class="px-4 flex justify-end mt-1 md:mt-3" onClick={handleMenuClick}>
                 <span>{lang==='english' ? 'Alarm system and locking up' : 'Sistema de alarma y bloqueo'}</span>          
             </Link>
         </li>
         <li class="py-2">
-            <Link to={'/appliances'} onClick={expandMenuClick} class="px-4 flex justify-end mt-1 md:mt-4">
+            <Link to={'/appliances'} onClick={expandMenuClick} class="px-4 flex justify-end mt-1 md:mt-3">
                 <span >{lang==='english' ? `Instructions for appliances and electronics ${changeChevron(expandMenu)}` : `Instrucciones para electrodomésticos y electrónica ${changeChevron(expandMenu)}`}</span>              
             </Link>
             </li>
@@ -95,32 +95,32 @@ return(
                    : null }
         
         <li class="py-2">
-            <Link to={'/towels'} class="px-4 flex justify-end mt-1 md:mt-4" onClick={handleMenuClick}>
+            <Link to={'/towels'} class="px-4 flex justify-end mt-1 md:mt-3" onClick={handleMenuClick}>
                 <span>{lang==='english' ? 'Towels, linen & kitchen supplies' : 'Toallas, ropa de cama y utensilios de cocina'}</span>               
             </Link>
         </li>
         <li class="py-2">
-            <Link to={'/waste'} class="px-4 flex justify-end mt-1 md:mt-4" onClick={handleMenuClick}>
+            <Link to={'/waste'} class="px-4 flex justify-end mt-1 md:mt-3" onClick={handleMenuClick}>
                 <span>{lang==='english' ? 'Waste disposal and recycling' : 'Eliminación y reciclaje de residuos'}</span>                      
             </Link>
         </li>
         <li class="py-2">
-            <Link to={'/information'} class="px-4 flex justify-end mt-1 md:mt-4" onClick={handleMenuClick}>
+            <Link to={'/information'} class="px-4 flex justify-end mt-1 md:mt-3" onClick={handleMenuClick}>
                 <span>{lang==='english' ? 'Information on the local area' : 'Información sobre el área local'}</span>               
             </Link>
         </li>
         <li class="py-2">
-            <Link to={'/contract'} class="px-4 flex justify-end mt-1 md:mt-4" onClick={handleMenuClick}>
+            <Link to={'/contract'} class="px-4 flex justify-end mt-1 md:mt-3" onClick={handleMenuClick}>
                 <span>{lang==='english' ? 'House rules & contract' : 'Reglas de la casa y contrato'}</span>              
             </Link>
         </li>
         <li class="py-2">
-            <Link to={'/cleaning'} class="px-4 flex justify-end mt-1 md:mt-4" onClick={handleMenuClick}>
+            <Link to={'/cleaning'} class="px-4 flex justify-end mt-1 md:mt-3" onClick={handleMenuClick}>
                 <span>{lang==='english' ? 'House cleaning' : 'Limpieza de la casa'}</span>             
             </Link>
         </li>
         <li class="py-2">
-            <Link to={'/feedback'} class="px-4 flex justify-end mt-1 md:mt-4" onClick={handleMenuClick}>
+            <Link to={'/feedback'} class="px-4 flex justify-end mt-1 md:mt-3" onClick={handleMenuClick}>
                 <span>{lang==='english' ? 'Feedback' : 'Retroalimentación'}</span>               
             </Link>
         </li>
