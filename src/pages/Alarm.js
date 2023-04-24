f7182944e17b4ed83fb02e819ca9008827bdcff1
Alfie Collins <alfiecollins44@gmail.com>
2023-04-24 14:17:05 +0100

    changes to alarm page

--- src/pages/Alarm.js
+++ src/pages/Alarm.js
@@ -32,17 +32,21 @@ return(
 
 <h2 class='text-xl font-bold mb-4 mt-8'> {lang==='english' ? `Keys` : 'Llaves'} </h2>
 
-<p>{lang==='english' ? 'We will provide a key for the front door on your arrival.' : 'Les proporcionaremos una llave para la puerta de entrada a su llegada.'}</p>
-<br/>
-<p>{lang==='english' ? 'The back door keys are by the back door.' : 'Las llaves de la puerta trasera están junto a la puerta trasera.'}</p>
-<br/>
-<p>{lang==='english' ? 'When you leave the building we suggest you do this by the back door:' : 'Cuando salen del edificio, le sugerimos que lo hagan por la puerta trasera:'}</p>
+
+<p>{lang==='english' ? 'When you leave the building, we suggest you do this by the back door:' : 'Cuando salga del edificio, le sugerimos que lo haga por la puerta trasera:'}</p>
 <br/>
 <ul>
-     <li class='ml-8'>{lang==='english' ? '•	Make sure the back door, conservatory doors, garden room doors and all ground floor windows are closed and locked.' : '•	Asegúrese de que la puerta trasera, las puertas de la habitación acristalada, las puertas de la sala del jardín y todas las ventanas de la planta baja estén cerradas con llave.'}</li>
+     <li class='ml-8'>{lang==='english' ? '•	Make sure the back door, conservatory doors, garden room doors and all ground floor windows are closed and locked.' : '•	Asegúrese de que la puerta trasera, las puertas del invernadero, las puertas de la sala del jardín y todas las ventanas de la planta baja estén cerradas y bloqueadas.'}</li>
      <br/>
      <li class='ml-8'>{lang==='english' ? '•	Exit by the back door, locking it after you and take the key with you.' : '•	Sal por la puerta trasera, ciérrala y llévate la llave.'}</li>
 </ul>
+<br/>
+
+<p>{lang==='english' ? `The backdoor keys are by the backdoor` : 'Las llaves de la puerta trasera están junto a la puerta trasera.'}</p>
+<br/>
+
+<p>{lang==='english' ? `When you arrive, we will show you the external key safe. We will set an access number that is memorable to you. In the key safe is a front door key. We suggest you leave it there unless there is no other way into the house. You can also leave back door keys in the key safe if you do not want to take them with you.` : 'Cuando llegue, le mostraremos la caja fuerte externa para llaves. Estableceremos un número de acceso que sea memorable para usted. En la caja fuerte de llaves hay una llave de la puerta de entrada. Le sugerimos que lo deje allí a menos que no haya otra forma de entrar a la casa. También puede dejar las llaves de la puerta trasera en la caja fuerte si no desea llevárselas.'}</p>
+
 
 <h2 class='text-xl font-bold mb-4 mt-8'> {lang==='english' ? `Alarm` : 'Alarma'} </h2>
 <p>{lang==='english' ? 'We live in a quiet neighbourhood, with very little trouble, so we suggest you do not use the alarm.' : 'Vivimos en un barrio tranquilo, con muy pocos problemas, por lo que le sugerimos que no use la alarma.'}</p>
